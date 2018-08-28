a8682490cf26c3c4287a8a28c291e684a07c0355
Kaicheng Yan <kaicheng.yan.1997@gmail.com>
2018-08-28 16:41:32 +0800

    add fields to participant

--- src/Models/Participant.php
+++ src/Models/Participant.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model {
     protected $table = 'participants';
-    protected $fillable = ['name', 'email'];
+    protected $fillable = ['name', 'email', 'company', 'phone'];
     public $timestamps = false;
 
     public function event() {
